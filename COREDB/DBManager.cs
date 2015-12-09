6bbe73bf8c54489ea793040ab3d2234a4ab687f5
Arnaud DA SILVA <danaru@openaliasbox.org>
2015-12-09 16:16:54 +0100

    DBInstaller

--- COREDB/DBManager.cs
+++ COREDB/DBManager.cs
@@ -29,6 +29,43 @@ namespace COREDB
 
         }
 
+        public static bool InstallDB()
+        {
+            
+            bool existFolderMathias = Directory.Exists("C:\\MATHIAS");
+            bool existDatabaseFodler = Directory.Exists("C:\\MATHIAS\\database");
+            bool exist = File.Exists("C:\\MATHIAS\\database\\mathias.sqlite");
+
+            if (!existFolderMathias)
+            {
+                Console.WriteLine("Création du dossier d'installation MATHIAS");
+                Directory.CreateDirectory("C:\\MATHIAS");
+            }
+            if (!existDatabaseFodler)
+            {
+                Console.WriteLine("Création du dossier database");
+                Directory.CreateDirectory("C:\\MATHIAS\\databse");
+            }
+            if (!exist)
+            {
+                Console.WriteLine("Création du fichier de la base de données sqlite.");
+                SQLiteConnection.CreateFile("C:\\MATHIAS\\database\\mathias.sqlite");
+            }
+            Console.WriteLine("Connection à la base nouvellement créé...");
+            SQLiteConnection sqlClient = new SQLiteConnection("Data Source = C:\\MATHIAS\\database\\mathias.sqlite; Version = 3;");
+
+            Console.WriteLine("Création des tables et des enregistrements");
+            string line;
+            StreamReader file = new StreamReader("Scripts/InstallDB.ini");
+            while ((line = file.ReadLine()) != null)
+            {
+                string queries = File.ReadAllText(Path.Combine("Scripts", line));
+                queries = queries.Replace("\n", "");
+                sqlClient.Execute(queries);
+            }
+            Console.WriteLine("Fin de l'installation de la base de données Mathias");
+            return true;
+        }
         private void CreateDataBase()
         {
             string line;
---
+++ DBInstaller/Program.cs
@@ -0,0 +1,19 @@
+﻿using COREDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installation de la base de données en cours...");
+            DBManager.InstallDB();
+            Console.WriteLine("Installation terminée, merci de rester sympa avec votre nouvelle IA");
+        }
+    }
+}
